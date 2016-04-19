95116c6bfb41a50e2d2887df24545786864bbdb6
Elaniin Digital <hello@elaniin.com>
2016-04-19 16:12:54 -0600

    Update comments

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 // function to verify session status
-// función para verificar el estado de ls sesión
 function is_session_started()
 {
     if ( php_sapi_name() !== 'cli' ) {
@@ -13,7 +12,6 @@ function is_session_started()
     return FALSE;
 }
 // verifying POST data and adding the values to session variables
-// verificando los datos POST y añadiendo los valores a variables de sesión
 if(isset($_POST["code"])){
   session_start();
   $_SESSION["code"] = $_POST["code"];
@@ -21,12 +19,10 @@ if(isset($_POST["code"])){
 
   $ch = curl_init();
   // Set url elements
-  // Estableciento elementos de la url
   $fb_app_id = '465871913602533';
   $ak_secret = 'eab92d7c75f08c6e95a48341c80b3ffc';
   $token = 'AA|'.$fb_app_id.'|'.$ak_secret;
   // Get access token
-  // Obteniendo access token
   $url = 'https://graph.accountkit.com/v1.0/access_token?grant_type=authorization_code&code='.$_POST["code"].'&access_token='.$token;
 
   $ch = curl_init();
@@ -39,7 +35,6 @@ if(isset($_POST["code"])){
   $info = json_decode($result);
 
   // Get account information
-  // Obteniendo información de la cuenta
   $url = 'https://graph.accountkit.com/v1.0/me/?access_token='.$info->access_token;
 
   $ch = curl_init();
@@ -54,24 +49,22 @@ if(isset($_POST["code"])){
 ?>
 <html>
 <head>
-	<title>Iniciar Sesión con AccountKit</title>
+	<title>Login with AccountKit</title>
   <link rel="shortcut icon" href="ak-icon.png">
   <link rel="stylesheet" href="css.css">
   <!--Hotlinked Account Kit SDK-->
-  <!--Account Kit SDK importado directamente-->
-  <script src="https://sdk.accountkit.com/es_ES/sdk.js"></script>
+  <script src="https://sdk.accountkit.com/en_EN/sdk.js"></script>
 </head>
 <body>
 <?php
 // verifying if the session exists
-// verificando si la sesión existe
 if(is_session_started() === FALSE && !isset($_SESSION)){
 ?>
-<h1 class="ac">Iniciar Sesión con AccountKit</h1>
-<p class="ac">Este ejemplo te muestra como implementar<br>Facebook AccountKit para web usando PHP.</p>
+<h1 class="ac">Login with AccountKit</h1>
+<p class="ac">This example shows you how to implement<br>Facebook AccountKit for web using PHP.</p>
 <div class="buttons">
-  <button onclick="phone_btn_onclick();">Iniciar Sesión con SMS</button>
-  <button onclick="email_btn_onclick();">Iniciar Sesión con Email</button>
+  <button onclick="phone_btn_onclick();">Login with SMS</button>
+  <button onclick="email_btn_onclick();">Login with Email</button>
 </div>
 <form action="" method="POST" id="my_form">
   <input type="hidden" name="code" id="code">
@@ -80,18 +73,17 @@ if(is_session_started() === FALSE && !isset($_SESSION)){
 <?php
 }else{
 ?>
-<h1 class="ac">Iniciar Sesión con AccountKit</h1>
-<p class="ac">La sesión con Facebook AccountKit ya está iniciada.</p>
+<h1 class="ac">Login with AccountKit</h1>
+<p class="ac">The session with Facebook AccountKit is already started.</p>
 <h3 class="ac">Tu Información</h3>
 <p class="ac">
   <!-- show account information -->
-  <!-- mostrar información de la cuenta -->
   <strong>ID:</strong> <?=$final->id?> <br>
-  <strong>Código de País:</strong> +<?=$final->phone->country_prefix?> <br>
-  <strong>Número de Teléfono:</strong> <?=$final->phone->national_number?> 
+  <strong>Country Code:</strong> +<?=$final->phone->country_prefix?> <br>
+  <strong>Phone Number:</strong> <?=$final->phone->national_number?> 
 </p>
 <div class="buttons">
-  <button onclick="logout();">Cerrar Sesión</button>
+  <button onclick="logout();">Logout</button>
 </div>
 <?php
 }
@@ -99,7 +91,6 @@ if(is_session_started() === FALSE && !isset($_SESSION)){
 </body>
 <script>
   // initialize Account Kit with CSRF protection
-  // inicialización de Account Kit con protección CSRF
   AccountKit_OnInteractive = function(){
     AccountKit.init(
       {
@@ -107,7 +98,6 @@ if(is_session_started() === FALSE && !isset($_SESSION)){
         state:"abcd", 
         version:"v1.0"
       }
-      //Si tu configuración en Account Kit lo requiere, debes incluir el parametro app_secret arriba
       //If your Account Kit configuration requires app_secret, you have to include ir above
     );
   };
@@ -133,15 +123,13 @@ if(is_session_started() === FALSE && !isset($_SESSION)){
   // phone form submission handler
   function phone_btn_onclick() {
     // you can add countryCode and phoneNumber to set values
-    // puedes añadir countryCode y phoneNumber para establecer valores
-    AccountKit.login('PHONE', // will use default values if this is not specified * Se utilizarán los valores predeterminados si estos no son especificados
+    AccountKit.login('PHONE', // will use default values if this is not specified
       loginCallback);
   }
 
   // email form submission handler
   function email_btn_onclick() {  
-    // you can add emailAddress   to set value
-    // puedes añadir emailAddress para establecer su valor
+    // you can add emailAddress to set value
     AccountKit.login('EMAIL', loginCallback);
   }
 
